3e6bab5b200c1fe65d82fea677a6ca30d20a92cc
rinfuyu <36916353+rinfuyu@users.noreply.github.com>
2018-12-12 19:48:25 +0900

    split

--- src/koma/gui/dialog/file/save/saveStream.kt
+++
@@ -1,9 +0,0 @@
-package koma.gui.dialog.file.save
-
-import okhttp3.HttpUrl
-
-fun HttpUrl.guessFileName(): String {
-    val ps = this.encodedPathSegments()
-    val ls = ps.getOrNull(ps.lastIndex)
-    return ls ?: ""
-}
