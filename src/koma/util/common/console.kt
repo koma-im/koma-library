b7bc6801e94f680ee1d2d7b0ab543eec424b62db
rinfuyu <36916353+rinfuyu@users.noreply.github.com>
2018-12-21 12:49:08 +0900

    clean

--- src/koma/util/common/console.kt
+++
@@ -1,5 +0,0 @@
-package koma.util.common
-
-inline fun eprintln(message: String) {
-    System.err.println(message)
-}
--- src/koma/util/time/fmt.kt
+++
@@ -1,9 +0,0 @@
-package koma.util.time
-
-import java.time.Instant
-import java.time.LocalDate
-import java.time.ZoneOffset
-
-fun utcDateFrom(inst: Long): LocalDate {
-    return Instant.ofEpochMilli(inst).atOffset(ZoneOffset.UTC).toLocalDate()
-}
