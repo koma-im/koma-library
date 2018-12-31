f2ee65b94ea180424d241bd13198359ce3cb4b6c
rinfuyu <36916353+rinfuyu@users.noreply.github.com>
2018-12-31 16:11:15 +0900

    fix config

--- src/koma/koma.kt
+++ src/koma/koma.kt
@@ -4,10 +4,10 @@ import koma.koma_app.SaveJobs
 import koma.network.client.okhttp.AppHttpClient
 import koma.storage.config.ConfigPaths
 import koma.storage.config.server.ServerConfStore
+import java.net.Proxy
 
-class Koma(data_dir: String) {
-    val paths = ConfigPaths(data_dir)
-    val http = AppHttpClient(paths, this)
+class Koma(val paths: ConfigPaths, proxy: Proxy) {
+    val http = AppHttpClient(paths, this, proxy)
     val servers = ServerConfStore(paths)
 
     /**
