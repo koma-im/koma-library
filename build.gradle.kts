f05e220cfd456c86e5199b42a1962b4706601d4e
yuforia <uforia@continuum.link>
2019-06-30 12:35:35 +0900

    switch to kotlin build script

---
+++ build.gradle.kts
@@ -0,0 +1,55 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("java")
+    id("maven")
+    id("org.jetbrains.kotlin.jvm").version("1.3.40")
+    id("org.jetbrains.kotlin.kapt").version("1.3.40")
+}
+
+repositories {
+    mavenLocal()
+    maven ("https://dl.bintray.com/jerady/maven")
+    maven("https://dl.bintray.com/kittinunf/maven")
+    maven("https://jitpack.io")
+    maven("https://repo.maven.apache.org/maven2")
+}
+
+tasks {
+    val sourcesJar by creating(Jar::class) {
+        archiveClassifier.set("sources")
+        from(sourceSets.main.get().allSource)
+    }
+    
+    artifacts {
+        archives(sourcesJar)
+    }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.40")
+    compileOnly("org.jetbrains.kotlin:kotlin-reflect:1.3.40")
+    compile("com.squareup.moshi:moshi:1.8.0")
+    implementation("org.cache2k:cache2k-core:1.0.2.Final")
+    compile("com.squareup.retrofit2:retrofit:2.5.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.5.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.1.1")
+    implementation("com.github.kittinunf.result:result:2.0.0")
+    implementation("io.github.microutils:kotlin-logging:1.6.22")
+    implementation("org.slf4j:slf4j-api:1.8.0-beta2")
+    implementation("org.jetbrains.kotlin:kotlin-test:1.3.40")
+    kapt("com.squareup.moshi:moshi-kotlin-codegen:1.8.0")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.2")
+    testRuntime("org.slf4j:slf4j-simple:1.8.0-beta2")
+}
+
+group = "io.github.koma-im"
+version = "0.7.20"
+description = "koma-library"
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    freeCompilerArgs = listOf("-XXLanguage:+InlineClasses")
+}
