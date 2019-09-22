510c1d7de0e3b01ed46d0f74864a130674304411
yuforia <uforia@continuum.link>
2019-09-22 22:03:37 +0900

    update library

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+version = "0.8.15"
+
 plugins {
     id("java")
     id("maven")
-    id("org.jetbrains.kotlin.jvm").version("1.3.41")
-    id("org.jetbrains.kotlin.kapt").version("1.3.41")
+    id("org.jetbrains.kotlin.jvm") version "1.3.50"
+    id("org.jetbrains.kotlin.kapt") version "1.3.50"
 }
 
 repositories {
@@ -27,25 +29,23 @@ tasks {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.41")
-    compileOnly("org.jetbrains.kotlin:kotlin-reflect:1.3.41")
+    implementation(kotlin("stdlib-jdk8"))
+    compileOnly(kotlin("reflect"))
     compile("com.squareup.moshi:moshi:1.8.0")
     compile("com.squareup.retrofit2:retrofit:2.6.1")
     implementation("com.squareup.retrofit2:converter-moshi:2.6.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.0-M2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.1")
     implementation("io.github.microutils:kotlin-logging:1.6.22")
     implementation("org.slf4j:slf4j-api:1.8.0-beta2")
-    implementation("org.jetbrains.kotlin:kotlin-test:1.3.41")
     kapt("com.squareup.moshi:moshi-kotlin-codegen:1.8.0")
 
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+    testImplementation(kotlin("test"))
+    testImplementation(kotlin("test-junit"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.2")
     testRuntime("org.slf4j:slf4j-simple:1.8.0-beta2")
 }
 
 group = "io.github.koma-im"
-version = "0.8.14"
 description = "koma-library"
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
