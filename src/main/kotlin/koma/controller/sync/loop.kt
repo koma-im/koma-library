41f1b1edba7518e566c857fde88ed6699261a88a
yuforia <uforia@continuum.link>
2020-01-21 12:19:51 +0900

    remove computer suspend detection
    MonoClock doesn't keep going when the computer is sleeping
    so it doesn't work

--- src/main/kotlin/koma/controller/sync/loop.kt
+++ src/main/kotlin/koma/controller/sync/loop.kt
@@ -6,7 +6,6 @@ import koma.matrix.sync.SyncResponse
 import koma.util.testFailure
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.selects.select
 import mu.KotlinLogging
 import kotlin.time.MonoClock
 import kotlin.time.seconds
@@ -15,32 +14,11 @@ import koma.util.KResult as Result
 private val logger = KotlinLogging.logger {}
 
 /**
- * detect computer suspend and resume and restart sync
- */
-fun CoroutineScope.detectTimeLeap(): Channel<Unit> {
-    val timeleapSignal = Channel<Unit>(Channel.CONFLATED)
-    var prev = MonoClock.markNow()
-    launch {
-        while (true) {
-            delay(1000)
-            val elapsed = prev.elapsedNow()
-            val now = prev + elapsed
-            if (elapsed.inSeconds > 20) {
-                logger.info { "System time leapt from $prev to $now" }
-                timeleapSignal.send(Unit)
-            }
-            prev = now
-        }
-    }
-    return timeleapSignal
-}
-
-
-/**
  * get events using the sync api
  * it stops when there is an exception
  * it is up to the caller to restart the sync
  */
+@Deprecated("use flows")
 class MatrixSyncReceiver(private val client: MatrixApi, var since: String?
 ) {
     /**
@@ -49,38 +27,23 @@ class MatrixSyncReceiver(private val client: MatrixApi, var since: String?
     val events = Channel<Result<SyncResponse, Failure>>(3)
 
     suspend fun startSyncing() = coroutineScope {
-        // check whether the computer was not running for some time
-        val timeCheck = detectTimeLeap()
         var timeout = 50.seconds
         launch {
-            sync@ while (true) {
+            while (true) {
                 val startTime = MonoClock.markNow()
-                val apiRes = async { client.sync(since, timeout = timeout) }
-                val ss: SyncStatus = select<SyncStatus> {
-                    apiRes.onAwait { SyncStatus.Response(it) }
-                    timeCheck.onReceive { SyncStatus.Resync() }
-                }
-                when (ss) {
-                    is SyncStatus.Response -> {
-                        val (it, e, res) = ss.response
-                        events.send(res)
-                        if (!res.testFailure(it, e)) {
-                            timeout = 50.seconds
-                            since = it.next_batch
-                        }else {
-                            timeout = 1.seconds
-                            logger.warn { "Sync failure: $e" }
-                            val minInterval = 1.seconds // limit rate of retries
-                            val dur= startTime.elapsedNow()
-                            if (dur < minInterval) {
-                                val remaining = minInterval - dur
-                                delay(remaining.toLongMilliseconds())
-                            }
-                        }
-                    }
-                    is SyncStatus.Resync -> {
-                        logger.info { "Restarting sync" }
-                        apiRes.cancel()
+                val (it, e, res) = client.sync(since, timeout = timeout)
+                events.send(res)
+                if (!res.testFailure(it, e)) {
+                    timeout = 50.seconds
+                    since = it.next_batch
+                }else {
+                    timeout = 1.seconds
+                    logger.warn { "Sync failure: $e" }
+                    val minInterval = 1.seconds // limit rate of retries
+                    val dur= startTime.elapsedNow()
+                    if (dur < minInterval) {
+                        val remaining = minInterval - dur
+                        delay(remaining.toLongMilliseconds())
                     }
                 }
             }
